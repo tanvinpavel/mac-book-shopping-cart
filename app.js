4c6251bb4ed993a85d9eacb28f0624d63e5b8c1e
tanvinpavel <tanvinpavel123@gmail.com>
2021-08-21 03:33:28 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+function addPrice(price, fieldName){
+    const fieldElement = document.getElementById(fieldName+'-field-id');
+    fieldElement.innerText = price;
+}
+
+const customizeBtn = document.getElementsByClassName('btn-outline-dark');
+
+for(element of customizeBtn){
+    element.addEventListener('click', function(event){
+        const btnName = event.target.innerText;
+        if(btnName == '8GB unified memory'){
+            // console.log('first');
+            addPrice(0, "memory");
+        }else if(btnName == '16GB unified memory'){
+            addPrice(180, "memory");
+        }else if(btnName == "256GB SSD storage"){
+            addPrice(0, "storage");
+        }else if(btnName == "512GB SSD storage"){
+            addPrice(100, "storage");
+        }else if(btnName == "1TB SSD storage"){
+            addPrice(180, "storage");
+        }else if(btnName == "Friday, Aug 25 FREE Prime Delivery"){
+            addPrice(0, "delivery");
+        }else if(btnName == "Friday, Aug 21 Delivery charge $20"){
+            addPrice(20, "delivery");
+        }
+        //count total price
+        totalPrice();
+    });
+}
+
+function totalPrice(){
+    let totalPrice = 0;
+    const productPrice = document.getElementsByClassName("priceField");
+    for(product of productPrice){
+        const eachProductPrice = parseFloat(product.innerText);
+        totalPrice = totalPrice + eachProductPrice;
+    }
+    document.getElementById('total-price').innerText = totalPrice;
+    document.getElementById('discount-price').innerText = totalPrice;
+}
+
+document.getElementById('promoCodeBtn').addEventListener('click', function(){
+    const promoCodeInput = document.getElementById('promoCodeInput');
+    const userInputPormoCode = promoCodeInput.value;
+    const netPrice = parseFloat(document.getElementById("total-price").innerText);
+    if(userInputPormoCode == "stevekaku"){
+        const discountPrice = netPrice - (netPrice * 0.2);
+        document.getElementById('discount-price').innerText = discountPrice;
+    }
+    promoCodeInput.value = "";
+})
